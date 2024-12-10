2acddd97e2ca6fce183d38a1fa7b9c6de85e213d
theheuman <theheuman@users.noreply.github.com>
2024-12-10 18:14:42 -0500

    Feature/scaffold-commands (#24)
    * scaffolding for all commands, some more complete than others
    * test directory structure also ready for test files

---
+++ src/commands/admin/scrim-crud/compute-scrim.ts
@@ -0,0 +1,56 @@
+import {
+  SlashCommandBuilder,
+  PermissionFlagsBits,
+  ChatInputCommandInteraction,
+} from "discord.js";
+import { signupsService } from "../../../services";
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("createscrimsignup") // Command name matching file name
+    .setDescription("Creates a new scrim signup text channel")
+    .addStringOption((option) =>
+      option
+        .setName("overstat-link")
+        .setDescription(
+          "Full length url of the completed scrim (not short url)",
+        )
+        .setMinLength(20)
+        .setMaxLength(150)
+        .setRequired(true),
+    )
+    .addNumberOption((option) =>
+      option.setName("skill").setDescription("Skill level of the lobby"),
+    )
+    // You will usually only want users that can create new channels to
+    // be able to use this command and this is what this line does.
+    // Feel free to remove it if you want to allow any users to
+    // create new channels
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels)
+    // It's impossible to create normal text channels inside DMs, so
+    // it's in your best interest in disabling this command through DMs
+    // as well. Threads, however, can be created in DMs, but we will see
+    // more about them later in this post
+    .setDMPermission(false),
+  async execute(interaction: ChatInputCommandInteraction) {
+    // Before executing any other code, we need to acknowledge the interaction.
+    // Discord only gives us 3 seconds to acknowledge an interaction before
+    // the interaction gets voided and can't be used anymore.
+    await interaction.reply({
+      content: "Fetched all input and working on your request!",
+    });
+    const channelId = interaction.channelId;
+    const overstatLink = interaction.options.getString("overstat-link", true);
+    const skill = interaction.options.getNumber("skill", true);
+
+    try {
+      await signupsService.computeScrim(channelId, overstatLink, skill);
+    } catch (error) {
+      await interaction.reply(`Unable to complete request: ${error}`);
+    }
+
+    await interaction.reply(
+      "Scrim successfully computed, you can now compute another lobby or close the scrim",
+    );
+  },
+};
