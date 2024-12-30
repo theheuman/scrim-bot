8af2795397eda689dd8f047ce0796db3dd39b40f
TheHeuman <jacob.heuman@gmail.com>
2024-12-30 17:11:39 -0500

    fix test commands

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,5 +1,16 @@
 import { Command } from "./command";
 import { AddPrioCommand } from "./admin/prio/add-prio";
+import { ServerInfoCommand } from "./utility/server";
+import { PingCommand } from "./utility/ping";
+import { UserCommand } from "./utility/user";
 
 // TODO inject services to mock things even easier
-export const commands: Command[] = [new AddPrioCommand()];
+export const commands: Command[] = [
+  // test commands
+  new ServerInfoCommand(),
+  new PingCommand(),
+  new UserCommand(),
+
+  // custom commands
+  new AddPrioCommand(),
+];
--- src/commands/utility/user.ts
+++ src/commands/utility/user.ts
@@ -1,30 +1,29 @@
-import {
-  SlashCommandBuilder,
-  CommandInteraction,
-  GuildMember,
-} from "discord.js";
+import { CommandInteraction, GuildMember } from "discord.js";
+import { Command } from "../command";
 
-export const data = new SlashCommandBuilder()
-  .setName("user")
-  .setDescription("Provides information about the user.");
+export class UserCommand extends Command {
+  constructor() {
+    super("user", "Provides information about the user.");
+  }
 
-export async function execute(interaction: CommandInteraction): Promise<void> {
-  const member = interaction.member;
+  async run(interaction: CommandInteraction): Promise<void> {
+    const member = interaction.member;
 
-  if (member instanceof GuildMember) {
-    const joinedAt = member.joinedAt;
-    if (joinedAt) {
-      await interaction.reply(
-        `This command was run by ${interaction.user.username}, who joined on ${joinedAt}.`,
-      );
+    if (member instanceof GuildMember) {
+      const joinedAt = member.joinedAt;
+      if (joinedAt) {
+        await interaction.reply(
+          `This command was run by ${interaction.user.username}, who joined on ${joinedAt}.`,
+        );
+      } else {
+        await interaction.reply(
+          `This command was run by ${interaction.user.username}, but I couldn't determine when they joined.`,
+        );
+      }
     } else {
       await interaction.reply(
         `This command was run by ${interaction.user.username}, but I couldn't determine when they joined.`,
       );
     }
-  } else {
-    await interaction.reply(
-      `This command was run by ${interaction.user.username}, but I couldn't determine when they joined.`,
-    );
   }
 }
