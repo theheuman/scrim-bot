8773c740f27f1c90e2a8a38f78406042d868602a
TheHeuman <jacob.heuman@gmail.com>
2025-01-01 21:14:49 -0500

    new config file format

---
+++ src/config.ts
@@ -0,0 +1,19 @@
+import configJson from "../config.json";
+
+export interface AppConfig {
+  discord: {
+    token: string;
+    clientId: string;
+    guildId: string;
+  };
+  nhost: { adminSecret: string; subdomain: string; region: string };
+}
+
+export interface ConfigFile {
+  prod: AppConfig;
+  dev: AppConfig;
+}
+
+const configFile = configJson as ConfigFile;
+export const appConfig: AppConfig =
+  process.env.NODE_ENV === "prod" ? configFile.prod : configFile.dev;
