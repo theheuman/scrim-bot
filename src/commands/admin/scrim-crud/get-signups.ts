a1cb8d300e166352d562d25da945e88f7fc0d77a
TheHeuman <jacob.heuman@gmail.com>
2025-01-12 17:37:11 -0500

    get signups main list and wait list

--- src/commands/admin/scrim-crud/get-signups.ts
+++ src/commands/admin/scrim-crud/get-signups.ts
@@ -3,6 +3,7 @@ import { AdminCommand } from "../../command";
 import { CustomInteraction } from "../../interaction";
 import { ScrimSignups } from "../../../services/signups";
 import { AuthService } from "../../../services/auth";
+import { Player } from "../../../models/Player";
 
 export class GetSignupsCommand extends AdminCommand {
   constructor(
@@ -28,46 +29,32 @@ export class GetSignupsCommand extends AdminCommand {
     try {
       channelSignups = await this.signupService.getSignups(channelId);
     } catch (e) {
-      await interaction.reply(`Could not fetch signups ${e}`);
+      await interaction.editReply(`Could not fetch signups. ${e}`);
       return;
     }
 
     const { mainList, waitList } = channelSignups;
 
-    const formatTeams = (teams: ScrimSignup[], startIndex: number) => {
-      return teams
-        .map((signup, index) => {
-          const players = signup.players
-            .map((player) => `<@${player.id}>`)
-            .join(", ");
+    const message = `Main list.\n${this.formatTeams(mainList)}\n\n\nWait list.\n${this.formatTeams(waitList)}`;
+    await interaction.editReply(message);
+  }
 
-          return `${startIndex + index + 1}. ${signup.teamName}: ${players} Priority: ${signup.prio ?? 0}`;
-        })
-        .join("");
-    };
+  formatTeams(teams: ScrimSignup[]) {
+    return teams.map((team) => this.formatTeam(team)).join("\n");
+  }
 
-    const sendMessages = async (messages: string[]) => {
-      for (const message of messages) {
-        await interaction.followUp({ content: message, ephemeral: true });
-      }
-    };
+  formatTeam(team: ScrimSignup) {
+    const playerString = team.players
+      .map((player) => this.formatPlayer(player))
+      .join(" ");
+    const teamString = `${team.teamName}. Signed up by: ${this.formatPlayer(team.signupPlayer)}. Players: ${playerString}.`;
+    const prioString = team.prio
+      ? ` Prio: ${team.prio.amount}. ${team.prio.reasons}.`
+      : "";
+    return teamString + prioString;
+  }
 
-    const messages: string[] = [];
-    let currentMessage = "Signed up teams for one lobby:\n";
-
-    const addTeamsToMessages = (teams: ScrimSignup[], startIndex: number) => {
-      for (let i = 0; i < teams.length; i += 20) {
-        const chunk = teams.slice(i, i + 20);
-        currentMessage += formatTeams(chunk, startIndex + i);
-        messages.push(currentMessage);
-        currentMessage = "Waitlist or multiple lobbies:\n";
-      }
-    };
-
-    addTeamsToMessages(mainList, 0);
-    addTeamsToMessages(waitList, mainList.length);
-
-    await interaction.reply({ content: messages.shift()!, ephemeral: true });
-    await sendMessages(messages);
+  formatPlayer(player: Player) {
+    return `<@${player.id}>`;
   }
 }
---
+++ test/commands/admin/scrim-crud/get.test.ts
@@ -0,0 +1,142 @@
+import {
+  GuildMember,
+  InteractionEditReplyOptions,
+  InteractionReplyOptions,
+  InteractionResponse,
+  Message,
+  MessagePayload,
+} from "discord.js";
+import SpyInstance = jest.SpyInstance;
+import { CustomInteraction } from "../../../../src/commands/interaction";
+import { AuthMock } from "../../../mocks/auth.mock";
+import { AuthService } from "../../../../src/services/auth";
+import { ScrimSignupMock } from "../../../mocks/signups.mock";
+import { ScrimSignups } from "../../../../src/services/signups";
+import { CloseScrimCommand } from "../../../../src/commands/admin/scrim-crud/close-scrim";
+import { GetSignupsCommand } from "../../../../src/commands/admin/scrim-crud/get-signups";
+import { ScrimSignup } from "../../../../src/models/Scrims";
+
+describe("Get signups", () => {
+  let basicInteraction: CustomInteraction;
+  let member: GuildMember;
+  let replySpy: SpyInstance<
+    Promise<InteractionResponse<boolean>>,
+    [reply: string | InteractionReplyOptions | MessagePayload],
+    string
+  >;
+  let editReplySpy: SpyInstance<
+    Promise<Message<boolean>>,
+    [options: string | InteractionEditReplyOptions | MessagePayload],
+    string
+  >;
+  let getSignupsSpy: SpyInstance<
+    Promise<{ mainList: ScrimSignup[]; waitList: ScrimSignup[] }>,
+    [channelId: string],
+    string
+  >;
+
+  const fakeDate = new Date();
+
+  const teamCaptain1 = {
+    id: "teamCap1",
+    discordId: "teamCap1DiscordId",
+    displayName: "Team Captain 1",
+  };
+
+  const mainListTeams: ScrimSignup[] = [
+    {
+      teamName: "Main list team",
+      players: [teamCaptain1, teamCaptain1],
+      signupId: "",
+      signupPlayer: teamCaptain1,
+      date: fakeDate,
+      prio: {
+        amount: 1,
+        reasons: "League prio",
+      },
+    },
+    {
+      teamName: "Main list team 2",
+      players: [],
+      signupId: "",
+      signupPlayer: {
+        id: "teamCap2",
+        discordId: "teamCapDiscordId2",
+        displayName: "Team Captain 2",
+      },
+      date: fakeDate,
+    },
+  ];
+
+  const waitListTeams: ScrimSignup[] = [
+    {
+      teamName: "Wait list team",
+      players: [],
+      signupId: "",
+      signupPlayer: {
+        id: "teamCap3",
+        discordId: "teamCapDiscordId3",
+        displayName: "Team Captain 3",
+      },
+      date: fakeDate,
+      prio: {
+        amount: -1,
+        reasons: "Team captain is a known inter",
+      },
+    },
+  ];
+
+  let command: GetSignupsCommand;
+
+  const mockScrimSignups = new ScrimSignupMock();
+
+  beforeAll(() => {
+    basicInteraction = {
+      channelId: "forum thread id",
+      reply: jest.fn(),
+      editReply: jest.fn(),
+    } as unknown as CustomInteraction;
+    replySpy = jest.spyOn(basicInteraction, "reply");
+    editReplySpy = jest.spyOn(basicInteraction, "editReply");
+    getSignupsSpy = jest.spyOn(mockScrimSignups, "getSignups");
+    getSignupsSpy.mockImplementation(() => {
+      return Promise.resolve({
+        mainList: mainListTeams,
+        waitList: waitListTeams,
+      });
+    });
+  });
+
+  beforeEach(() => {
+    replySpy.mockClear();
+    editReplySpy.mockClear();
+    getSignupsSpy.mockClear();
+    command = new GetSignupsCommand(
+      new AuthMock() as AuthService,
+      mockScrimSignups as unknown as ScrimSignups,
+    );
+  });
+
+  it("Should get signups", async () => {
+    jest.useFakeTimers();
+    await command.run(basicInteraction);
+    expect(getSignupsSpy).toHaveBeenCalledWith("forum thread id");
+    const expectedString = `Main list.\nMain list team. Signed up by: <@teamCap1>. Players: <@teamCap1> <@teamCap1>. Prio: 1. League prio.\nMain list team 2. Signed up by: <@teamCap2>. Players: .\n\n\nWait list.\nWait list team. Signed up by: <@teamCap3>. Players: . Prio: -1. Team captain is a known inter.`;
+    expect(editReplySpy).toHaveBeenCalledWith(expectedString);
+    jest.runAllTimers();
+    jest.useRealTimers();
+  });
+
+  describe("errors", () => {
+    it("should not get signups because the signup service threw an exception", async () => {
+      getSignupsSpy.mockImplementation(() => {
+        throw Error("No scrim found for that channel");
+      });
+
+      await command.run(basicInteraction);
+      expect(editReplySpy).toHaveBeenCalledWith(
+        "Could not fetch signups. Error: No scrim found for that channel",
+      );
+    });
+  });
+});
