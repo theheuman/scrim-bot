9dcd9e197052c600f178f3aed2b4d5b938b1fcdc
TheHeuman <jacob.heuman@gmail.com>
2025-01-19 22:17:38 -0500

    reply immediately to add prio command

--- src/commands/admin/prio/add-prio.ts
+++ src/commands/admin/prio/add-prio.ts
@@ -51,6 +51,11 @@ export class AddPrioCommand extends AdminCommand {
       this.inputNames.endDate,
       true,
     );
+
+    await interaction.reply({
+      content: "Fetched all input and working on your request!",
+    });
+
     // end date is inclusive
     endDate = setEasternHours(endDate, 23, 59, 59);
 
@@ -65,14 +70,14 @@ export class AddPrioCommand extends AdminCommand {
         reason,
       );
     } catch (e) {
-      await interaction.reply("Error while executing set prio: " + e);
+      await interaction.editReply("Error while executing set prio: " + e);
       return;
     }
 
     const prioIdString = dbIds
       .map((dbId, index) => `${users[index]?.displayName} prio id: ${dbId}`)
       .join("\n");
-    await interaction.reply(
+    await interaction.editReply(
       `Added ${amount} prio to ${users.length} player${users.length === 1 ? "" : "s"} from ${this.formatDate(startDate)} to ${this.formatDate(endDate)}\nReason: ${reason}.\nID's:\n${prioIdString}`,
     );
   }
--- test/commands/admin/prio/add-prio.test.ts
+++ test/commands/admin/prio/add-prio.test.ts
@@ -1,7 +1,9 @@
 import {
   GuildMember,
+  InteractionEditReplyOptions,
   InteractionReplyOptions,
   InteractionResponse,
+  Message,
   MessagePayload,
   Snowflake,
   User,
@@ -33,9 +35,9 @@ describe("Add prio", () => {
   >;
   let member: GuildMember;
   const supreme: User = { displayName: "Supreme", id: "1" } as unknown as User;
-  let replySpy: SpyInstance<
-    Promise<InteractionResponse<boolean>>,
-    [reply: string | InteractionReplyOptions | MessagePayload],
+  let editReplySpy: SpyInstance<
+    Promise<Message<boolean>>,
+    [reply: string | InteractionEditReplyOptions | MessagePayload],
     string
   >;
 
@@ -73,6 +75,7 @@ describe("Add prio", () => {
         getNumber: () => -400,
       },
       reply: jest.fn(),
+      editReply: jest.fn(),
       channelId,
       member,
     } as unknown as CustomInteraction;
@@ -100,6 +103,7 @@ describe("Add prio", () => {
         getNumber: () => -400,
       },
       reply: jest.fn(),
+      editReply: jest.fn(),
       channelId,
       member,
     } as unknown as CustomInteraction;
@@ -129,9 +133,9 @@ describe("Add prio", () => {
       },
     );
 
-    replySpy = jest.spyOn(singleUserInteraction, "reply");
+    editReplySpy = jest.spyOn(singleUserInteraction, "editReply");
     await command.run(singleUserInteraction);
-    expect(replySpy).toHaveBeenCalledWith(
+    expect(editReplySpy).toHaveBeenCalledWith(
       `Added -400 prio to 1 player from <t:${Math.floor(fakeDate.valueOf() / 1000)}:f> to <t:${Math.floor(new Date("2025-01-13T23:59:59-05:00").valueOf() / 1000)}:f>\nReason: Prio reason.\nID's:\nSupreme prio id: db id`,
     );
     // if this is failing, and you haven't changed the amount of assertions, take a look a little higher in the log to see if the setPlayerPrioSpy was called with differing values
@@ -155,9 +159,9 @@ describe("Add prio", () => {
       },
     );
 
-    replySpy = jest.spyOn(basicInteraction, "reply");
+    editReplySpy = jest.spyOn(basicInteraction, "editReply");
     await command.run(basicInteraction);
-    expect(replySpy).toHaveBeenCalledWith(
+    expect(editReplySpy).toHaveBeenCalledWith(
       `Added -400 prio to 3 players from ${command.formatDate(new Date("2025-01-12T00:00:00-05:00"))} to ${command.formatDate(new Date("2025-01-13T23:59:59-05:00"))}\nReason: Prio reason.\nID's:\nSupreme prio id: db id\nSupreme prio id: db id 2\nSupreme prio id: db id 3`,
     );
     // if this is failing, and you haven't changed the amount of assertions, take a look a little higher in the log to see if the setPlayerPrioSpy was called with differing values
@@ -169,9 +173,9 @@ describe("Add prio", () => {
       return Promise.reject("The database fell asleep");
     });
 
-    replySpy = jest.spyOn(basicInteraction, "reply");
+    editReplySpy = jest.spyOn(basicInteraction, "editReply");
     await command.run(basicInteraction);
-    expect(replySpy).toHaveBeenCalledWith(
+    expect(editReplySpy).toHaveBeenCalledWith(
       "Error while executing set prio: The database fell asleep",
     );
   });
