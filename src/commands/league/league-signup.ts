3a68899b33ff426e079b588efec1e32500ea2cf9
TheHeuman <jacob.heuman@gmail.com>
2025-10-06 12:54:50 -0400

    all inputs accounted for

--- src/commands/league/league-signup.ts
+++ src/commands/league/league-signup.ts
@@ -3,23 +3,47 @@ import { CustomInteraction } from "../interaction";
 import { isGuildMember } from "../../utility/utility";
 import { Player } from "../../models/Player";
 import { OverstatService } from "../../services/overstat";
+import { User } from "discord.js";
 
+// TODO which fields are optional
 export class LeagueSignupCommand extends MemberCommand {
-  teamNameInputName = "team-name";
-  compExperienceInputName = "comp-experience";
-  player1InputName = "player1";
-  player2InputName = "player2";
-  player3InputName = "player3";
+  inputNames = {
+    teamName: "team-name",
+    compExperience: "comp-experience",
+    daysUnableToPlay: "days-unable-to-play",
+
+    player1: {
+      user: "player1",
+      rank: "player1-rank",
+      lastSeasonDivision: "player1-last-season-vesa-division",
+      overstatLink: "player1-overstat-link",
+    },
+
+    player2: {
+      user: "player2",
+      rank: "player2-rank",
+      lastSeasonDivision: "player2-last-season-vesa-division",
+      overstatLink: "player2-overstat-link",
+    },
+
+    player3: {
+      user: "player3",
+      rank: "player3-rank",
+      lastSeasonDivision: "player3-last-season-vesa-division",
+      overstatLink: "player3-overstat-link",
+    },
+  };
 
   constructor(private overstatService: OverstatService) {
     super("league-signup", "Signup for the league");
-    this.addStringInput(this.teamNameInputName, "Team name", {
+    this.addStringInput(this.inputNames.teamName, "Team name", {
       isRequired: true,
       minLength: 1,
       maxLength: 25,
     });
+    // TODO add choices https://docs.discordnet.dev/guides/int_basics/application-commands/slash-commands/choice-slash-command.html
     this.addIntegerInput(
-      this.compExperienceInputName,
+      this.inputNames.compExperience,
       "Your teams comp experience: 1 for none, 5 for extremely experienced",
       {
         isRequired: true,
@@ -28,25 +52,74 @@ export class LeagueSignupCommand extends MemberCommand {
       },
     );
 
-    this.addUserInput(this.player1InputName, "@player1", true);
-    this.addUserInput(this.player2InputName, "@player2", true);
-    this.addUserInput(this.player3InputName, "@player3", true);
+    this.addStringInput(
+      this.inputNames.daysUnableToPlay,
+      "Days your team is unable to play, ex: Monday, Tuesday, Wednesday, Thursday, Friday. Input as a comma delimited list of days your team can not play because one or more of you have other engagements",
+    );
+
+    this.addUserInput(this.inputNames.player1.user, "@player1", true);
+    this.addUserInput(this.inputNames.player2.user, "@player2", true);
+    this.addUserInput(this.inputNames.player3.user, "@player3", true);
+
+    this.addStringInput(
+      this.inputNames.player1.overstatLink,
+      "Player 1 overstat link",
+    );
+    this.addStringInput(
+      this.inputNames.player2.overstatLink,
+      "Player 2 overstat link",
+    );
+    this.addStringInput(
+      this.inputNames.player3.overstatLink,
+      "Player 3 overstat link",
+    );
+
+    // TODO choice
+    this.addStringInput(
+      this.inputNames.player1.rank,
+      "Player 1 last seasons peak rank",
+    );
+    this.addStringInput(
+      this.inputNames.player2.rank,
+      "Player 2 last seasons peak rank",
+    );
+    this.addStringInput(
+      this.inputNames.player3.rank,
+      "Player 3 last seasons peak rank",
+    );
+
+    // TODO choice
+    this.addUserInput(
+      this.inputNames.player1.lastSeasonDivision,
+      "Player 1 last vesa seasons division",
+      true,
+    );
+    this.addUserInput(
+      this.inputNames.player2.lastSeasonDivision,
+      "Player 2 last vesa seasons division",
+      true,
+    );
+    this.addUserInput(
+      this.inputNames.player3.lastSeasonDivision,
+      "Player 3 last vesa seasons division",
+      true,
+    );
   }
 
   async run(interaction: CustomInteraction) {
     const channelId = interaction.channelId;
     const teamName = interaction.options.getString(
-      this.teamNameInputName,
+      this.inputNames.teamName,
       true,
     );
     const compExperience = interaction.options.getInteger(
-      this.compExperienceInputName,
+      this.inputNames.compExperience,
       true,
     );
     const signupPlayer = interaction.member;
-    const player1 = interaction.options.getUser(this.player1InputName, true);
-    const player2 = interaction.options.getUser(this.player2InputName, true);
-    const player3 = interaction.options.getUser(this.player3InputName, true);
+    const player1 = this.getPlayerInputs(this.inputNames.player1, interaction);
+    const player2 = this.getPlayerInputs(this.inputNames.player2, interaction);
+    const player3 = this.getPlayerInputs(this.inputNames.player3, interaction);
 
     if (!isGuildMember(signupPlayer)) {
       await interaction.reply(
@@ -56,5 +129,35 @@ export class LeagueSignupCommand extends MemberCommand {
     }
     await interaction.ogInteraction.deferReply();
     // do the logic!
+  }
+
+  // TODO convert integer inputs to enums
+  // TODO which ones should be optional?
+  getPlayerInputs(
+    playerNumberInputs: {
+      user: string;
+      rank: string;
+      overstatLink: string;
+      lastSeasonDivision: string;
+    },
+    interaction: CustomInteraction,
+  ): {
+    user: User;
+    rank: number;
+    overstatLink: string;
+    lastSeasonDivision: number;
+  } {
+    return {
+      user: interaction.options.getUser(playerNumberInputs.user, true),
+      rank: interaction.options.getInteger(playerNumberInputs.rank, true),
+      overstatLink: interaction.options.getString(
+        playerNumberInputs.overstatLink,
+        true,
+      ),
+      lastSeasonDivision: interaction.options.getInteger(
+        playerNumberInputs.lastSeasonDivision,
+        true,
+      ),
+    };
   }
 }
