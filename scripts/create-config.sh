005dc3b2a4bf2f389184f8a714f6683d83ddbbde
TheHeuman <jacob.heuman@gmail.com>
2025-12-29 22:48:35 -0500

    create google key file creation to create-config script

--- scripts/create-config.sh
+++ scripts/create-config.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-file_location=config.json
+config_file_location=config.json
+service_key_file_location=service-account-key.json
 
 warn_if_missing() {
   local var_name="$1"
@@ -14,12 +15,17 @@ warn_if_missing "LOBBY_SIZE" "20"
 warn_if_missing "DISCORD_BOT_TOKEN" "default_bot_token"
 warn_if_missing "DISCORD_CLIENT_ID" "default_client_id"
 warn_if_missing "DISCORD_GUILD_ID" "default_guild_id"
-warn_if_missing "DISCORD_GUILD_ID_LEAGUE" "default_guild_id"
+warn_if_missing "DISCORD_GUILD_ID_LEAGUE" "default_guild_id_league"
 warn_if_missing "NHOST_SECRET" "default_admin_secret"
 warn_if_missing "NHOST_SUBDOMAIN" "default_subdomain"
 warn_if_missing "NHOST_REGION" "default_region"
+warn_if_missing "GOOGLE_KEY_ID" "default_key_id"
+warn_if_missing "GOOGLE_KEY_FILE" "default_key_file"
+warn_if_missing "GOOGLE_KEY_EMAIL" "default_key_email"
+warn_if_missing "GOOGLE_CLIENT_ID" "default_client_id"
+warn_if_missing "GOOGLE_KEY_CERT_URL" "default_key_cert_url"
 
-cat > $file_location <<EOF
+cat > $config_file_location <<EOF
 {
   "dev": {
     "lobbySize": 3,
@@ -55,3 +61,23 @@ cat > $file_location <<EOF
   }
 }
 EOF
+
+jq -n \
+  --arg pk "$GOOGLE_KEY_FILE" \
+  --arg pkid "$GOOGLE_KEY_ID" \
+  --arg email "$GOOGLE_KEY_EMAIL" \
+  --arg cid "$GOOGLE_CLIENT_ID" \
+  --arg cert "$GOOGLE_KEY_CERT_URL" \
+  '{
+    type: "service_account",
+    project_id: "vesa-apex",
+    private_key_id: $pkid,
+    private_key: $pk,
+    client_email: $email,
+    client_id: $cid,
+    auth_uri: "https://accounts.google.com/o/oauth2/auth",
+    token_uri: "https://oauth2.googleapis.com/token",
+    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+    client_x509_cert_url: $cert,
+    universe_domain: "googleapis.com"
+  }' > "$service_key_file_location"
