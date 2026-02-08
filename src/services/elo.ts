9b808d0a5f771b79d4b32dea6428a857b76db66b
TheHeuman <jacob.heuman@gmail.com>
2026-02-08 13:59:43 -0500

    general idea for elo service

--- src/services/elo.ts
+++ src/services/elo.ts
@@ -1,18 +1,18 @@
 import { DB } from "../db/db";
 import {
-  OverstatTournamentResponse,
-  GameResponse,
-  PlayerGameStats,
+    OverstatTournamentResponse,
+    GameResponse,
+    PlayerGameStats,
 } from "../models/overstatModels";
 
 export class EloService {
-  constructor(private db: DB) {}
+    constructor(private db: DB) { }
 
-  // take in overstat json and use private methods to modify each players elo
-  // since this algorithm takes it game by game, store elo changes locally until whole set has been computed
-  calculateEloForLobby(overstatJson: OverstatTournamentResponse) {}
+    // take in overstat json and use private methods to modify each players elo
+    // since this algorithm takes it game by game, store elo changes locally until whole set has been computed
+    calculateEloForLobby(overstatJson: OverstatTournamentResponse) { }
 
-  private calculateEloForGame(gameJson: GameResponse) {}
-  private calculateLobbyRating(playerMMRs: number[]) {}
-  private calculatePlayerPerformanceScore(playerStats: PlayerGameStats) {}
+    private calculateEloForGame(gameJson: GameResponse) { }
+    private calculateLobbyRating(playerMMRs: number[]) { }
+    private calculatePlayerPerformanceScore(playerStats: PlayerGameStats) { }
 }
